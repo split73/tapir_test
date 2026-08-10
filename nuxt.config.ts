adc7b9fb075fb09be41c1d8b755debea0d20b713
split73 <112943845+split73@users.noreply.github.com>
2026-08-10 13:45:12 +0000

    github actions config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,21 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2026-02-16',
   devtools: { enabled: true },
   css: ['~/assets/styles/main.scss'],
   modules: ['@nuxt/eslint', 'nuxt-icons'],
-  nuxtIcons: {path: '~/app/assets/icons'}
+  nuxtIcons: { path: '~/app/assets/icons' },
+
+  ssr: false, 
+
+  app: {
+    baseURL: '/tapir_test/', 
+    buildAssetsDir: 'assets'
+  },
+
+  nitro: {
+    preset: 'github-pages',
+    prerender: {
+      routes: ['/']
+    }
+  }
 })
